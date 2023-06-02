e244d59172d78cd0f77ab6291e9aadaed3a7d71b
DelphineLecorney <lecorney.delphine@gmail.com>
2023-06-02 22:04:52 +0200

    Input radio

--- excuse.php
+++ excuse.php
@@ -15,13 +15,24 @@
     <input type="text" name="nameChild">
     <br>
     <label for="gender">Please select your gender: </label>
-    <input type="radio" name="gender" value="boy" id="boy" checked="checked" />
+    <input type="radio" name="gender" value="boy" id="boy" />
     <label for="boy">Boy</label>
     <input type="radio" name="gender" value="girl" id="girl" /> 
     <label for="girl">Girl</label>
-
+    <br>
     <label for="nameTeacher">Please enter the teacher's name :</label>
     <input type="text" name="nameTeacher">
+    <br>
+    <label for="gender">Please select an option : </label>
+    <br>
+    <input type="radio" name="options" value="illness" id="illness" />
+    <label for="illness">Illness</label>
+    <br>
+    <input type="radio" name="options" value="death-of-the-pet" id="death-of-the-pet" />
+    <label for="death-of-the-pet">Death-of-the-pet</label>
+    <br>
+    <input type="radio" name="options" value="death-of-the-family's member" id="death-of-the-family's member" />
+    <label for="death-of-the-family's member">Death-of-the-family's member</label>
 
 </form>
 <?php
